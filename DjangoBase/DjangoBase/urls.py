369918b213c5db1968b8eb0daac0ef4308417922
unknown <winchook@gmail.com>
2020-01-31 22:58:04 +0800

    类视图方法get post put delete

--- DjangoBase/DjangoBase/urls.py
+++ DjangoBase/DjangoBase/urls.py
@@ -15,17 +15,24 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,re_path
-from .views import hello
+from .views import * #这里的*表示导入所有的视图
 #from .views import user,sys,html
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     #形如这样的访问http://0.0.0.0:8000/hello/a
     #这里的括号表示加参数传给hello这个函数,点代表一个字符,+代表多个字符
-    re_path(r'^hello/(.+)/(.+)$', hello.as_view()),
+    #re_path(r'^hello/(.+)/(.+)$', hello.as_view()),
     #re_path(r'^user/(\w+)/(\d+)/$', user)
     #关键字参数如下表示,使用场景是给接口传递一个id
     #re_path(r'^user/(?P<PK>\d+)/$', user),#这里的PK在view里面也需要是PK
     #re_path(r'^sys/(?P<cmd>.+)/$', sys),
-    #re_path(r'^html/(?P<cmd>.+)/$', html)
+    #re_path(r'^html/(?P<cmd>.+)/$', html),
+    re_path(r'^hello/(?P<pk>\d+)/$', hello.as_view()),
+
+    #访问方式: http://106.53.124.157:8000/get_sys_func
+    re_path(r'get_sys_func', render_sys),
+
+    #访问方式: http://106.53.124.157:8000/get_sys_class
+    re_path(r'get_sys_class', RenderSys.as_view())
 ]
--- DjangoBase/DjangoBase/views.py
+++ DjangoBase/DjangoBase/views.py
@@ -58,7 +58,22 @@
 from django.http import HttpResponse,JsonResponse
 from django.shortcuts import render
 #导入基类模块
-from django.views.generic import View
+from django.views.generic import View, TemplateView
+#导入如下模块可以将request请求的结果转换为字典
+from django.http import request,QueryDict
+
+#函数视图
+def render_sys(request):
+    return render(request, 'sys.html')
+
+#类视图
+class RenderSys(TemplateView):
+    template_name = 'sys.html'
+    #不写get方法,是因为父类里面已经有了
+    #如果需要传递变量,只需要覆盖get_context_data方法即可
+    def get_context_data(self, **kwargs):
+        bio = 'qq we er rt tt ee'
+        return {'bio': bio}
 
 #定义类
 class hello(View):
@@ -74,4 +89,51 @@ class hello(View):
             'bio':bio
         }
         return render(request, 'sys.html', context)
+        #前端使用get请求
+        #res = request.get(usl)
+        #res.content
+
+    def post(self, request, *args, **kwargs):
+        #data = request.POST.get('n')#这样是QueryDict形式的
+        #转化为字典
+        #data = request.POST.dict()
+        #或者
+        data = QueryDict(request.body).dict()
+        print(args, kwargs)#打印位置参数args及关键字参数kwargs
+        print(data)
+        #前端使用post请求,可以使用一个变量来接收
+        #res = requests.post(url, data=data)
+        #res.content
+        #res.status_code
+        return JsonResponse({'desc':data})
+
+    #自定义方法,然后供get post put调用
+    def handle_data(self, data):
+        return data + '  123'
+
+    def put(self, request, *args, **kwargs):
+        data = QueryDict(request.body).dict()
+        print(data)
+        #调用handle_data方法
+        res = self.handle_data('hahh')
+        print(res)
+        return JsonResponse({'desc':data, 'desc define': res})
+        #前端使用put请求
+        #res = requests.put(url, data=data)
+#最后注意: 后端的方法和前端请求的方法需要对应起来。
+#特别注意: get post put方法名称是不能变的,可以自定义方法进行数据处理,
+#然后在get post put方法中调用它,例如上面的handle_data方法
+    def delete(self, requests, *args, **kwargs):
+        print(kwargs)#这里获取关键字参数pk的值
+        return JsonResponse({})
+        #前端使用delete请求
+        #url = 'http://106.53.124.157:8000/hello/1/'
+        #res = requests.delete(url)
+#说明: restful风格
+#       增删改查
+#       get:一般是获取资源
+#       post:一般是创建资源
+#       put: 一般是修改资源（覆盖修改）
+#       patch:一般是修改资源(部分修改)
+#       delete:一般是删除资源
 
