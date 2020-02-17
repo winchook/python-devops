edd6650653f4c361012cc65d24a44848b2f25e16
unknown <winchook@gmail.com>
2020-02-17 15:35:46 +0800

    models定义

--- DjangoBase/cmdb/models.py
+++ DjangoBase/cmdb/models.py
@@ -6,9 +6,11 @@ class Idc(models.Model):
     '''
     机房表
     '''
-    name = models.CharField(max_length=32)
+    name = models.CharField(max_length=32,null=True,blank=True)
     address = models.CharField(max_length=32)
     remark = models.TextField()
+    price = models.CharField(max_length=64,null=True,blank=True,default='0')
 
+    #这里表示是在Django后台显示的表,可有可无
     def __str__(self):#在python3用__str__代替__unicode__
         return self.name
