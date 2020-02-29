f23633e2fc471909e25f94d5069698e4ee952930
winchoo <winchook@gmail.com>
2020-03-01 00:32:41 +0800

    ListView使用

--- DjangoBase/cmdb/views.py
+++ DjangoBase/cmdb/views.py
@@ -1,15 +1,28 @@
 from django.shortcuts import render
-from django.views.generic import View, TemplateView
+from django.views.generic import View, TemplateView, ListView
 from django.http import JsonResponse, QueryDict
 from .models import *
 
 # Create your views here.
+#使用TemplateView实现
+'''
 class IdcView(TemplateView):
     template_name = 'cmdb/idcs.html'
 
     def get_context_data(self, **kwargs):
         idcs = Idc.objects.all()
         return {'idcs':idcs}
+'''
+#使用ListView实现
+class IdcView(ListView):
+    model = Idc
+    template_name = 'cmdb/idcs.html'
+    context_object_name = 'idcs'#这里的idcs是在cmdb/idcs.html中定义的列表变量
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(ListView, self).get_context_data(**kwargs)
+        context['version'] = '2.0.8' #这里的version是cmdb/idcs.html中的version变量
+        return context
 
     #使用post方法对数据库进行增加操作
     def post(self, request, *args, **kwargs):
